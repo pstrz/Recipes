51031177dbb617bd138ef7e204509804869c8cb0
pstrz <31897978+pstrz@users.noreply.github.com>
2018-04-21 11:30:22 -0700

    fsck

--- gitbucket_example.py
+++ gitbucket_example.py
@@ -5,10 +5,18 @@ Created on Sat Apr 21 09:32:30 2018
 @author: philstrzelecki
 """
 import numpy as np
+from git import Repo
 
 A = np.ones([3,3])
 np.save('A', A)
 
-from git import Repo
-
 repo = Repo("")
+
+origin = repo.remote()
+origin.pull()
+
+
+
+origin.push()
+
+#none of this works :) 
